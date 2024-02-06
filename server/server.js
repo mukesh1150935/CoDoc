7945630251de0463e9684fb9364c4ef2c487ca22
mukesh <mukesh1150935@gmail.com>
2024-02-06 15:43:06 +0530

    socket updated

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.listen(port,()=>{
 
 
 
-const io = require("socket.io")(3001, {
+const io = require("socket.io")(port, {
   cors: {
     origin: "https://codoc-mukesh.onrender.com",
     methods: ["GET", "POST"],
